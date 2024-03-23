81a7ec43cb4bfa97d67e229dabe43571ef9fcc00
Brendan <brendanwsmull@gmail.com>
2024-03-23 15:02:42 -0400

    Added Components

---
+++ app/src/components/Matches.js
@@ -0,0 +1,16 @@
+import React from 'react'
+//functional component (always try to use)
+// function Greet() {
+//     return <h1>Hello Brendan</h1>
+// }
+
+export const Matches = props => {
+    console.log(props)
+    return (
+        <div>
+        
+        </div>
+    )
+}
+
+export default Matches
