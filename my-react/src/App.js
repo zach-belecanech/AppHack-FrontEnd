7db398d1cd0511a71e1451af1873e4a184f0005d
Brendan <brendanwsmull@gmail.com>
2024-03-23 18:05:32 -0400

    Added dif sections

--- my-react/src/App.js
+++ my-react/src/App.js
@@ -11,13 +11,17 @@ function App() {
               <Link to="/">Home</Link>
             </li>
             <li>
-              <Link to="/about">About</Link>
+              <Link to="/Matches">Matches</Link>
+            </li>
+            <li>
+              <Link to="/Account">Account</Link>
             </li>
           </ul>
         </nav>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/Matches" element={<Matches />} />
+          <Route path="/Account" element={<Account />} />
         </Routes>
       </div>
     </Router>
@@ -28,8 +32,12 @@ function Home() {
   return <h2>Home Page</h2>;
 }
 
-function About() {
-  return <h2>About Page</h2>;
+function Matches() {
+  return <h2>Matches Page</h2>;
+}
+
+function Account() {
+  return <h2>Account Page</h2>;
 }
 
 export default App;
